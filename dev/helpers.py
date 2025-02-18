ac3d494931bcc5dc3069e044a6c4a19d66041bc3
MarkFowlerERG <Mark.Fowler@erg.com>
2025-02-18 10:55:37 -0500

    Update helpers.py

--- dev/helpers.py
+++ dev/helpers.py
@@ -7,3 +7,54 @@ Last Update: 01/21/2025
 Description: 
 """
 
+def file_select():
+    root = tk.Tk()
+    filenames = askopenfilenames()
+    root.destroy()
+    return filenames
+
+def dict_extract(inputFile):
+    im = Image.open(inputFile)
+    exif_dict = piexif.load(im.info.get('exif'))
+    exif_dict = exif_to_tag(exif_dict)
+    return(exif_dict)
+
+def decimal_degree(degree, minute, second):
+    value = degree + (minute/60) + (second/3600)
+    return value
+
+def exif_coordinates(inputDict):
+    latitudes = inputDict['GPS'].get("GPSLatitude")
+    if latitudes is None:
+        inputLatDeg = 0
+        inputLatMin = 0
+        inputLatSec = 0
+    else:
+        inputLatDeg = latitudes[0][0]
+        inputLatMin = latitudes[1][0]
+        inputLatSec = latitudes[2][0]/1000
+    if inputDict['GPS'].get("GPSLatitudeRef") == 'S':
+        latHemisphere = -1
+    else:
+        latHemisphere = 1
+    outputLat = decimal_degree(inputLatDeg,inputLatMin,inputLatSec) *latHemisphere
+    longitudes = inputDict['GPS'].get("GPSLongitude")
+    if longitudes is None:
+        inputLonDeg = 0
+        inputLonMin = 0
+        inputLonSec = 0
+    else:
+        inputLonDeg = longitudes[0][0]
+        inputLonMin = longitudes[1][0]
+        inputLonSec = longitudes[2][0]/1000
+    if inputDict['GPS'].get("GPSLongitudeRef") == 'W':
+        lonHemisphere = -1
+    else:
+        lonHemisphere = 1
+    outputLon = decimal_degree(inputLonDeg,inputLonMin,inputLonSec) *lonHemisphere
+    inputBearing = inputDict['GPS'].get('GPSImgDirection')
+    if inputBearing[1] !=0:
+        outputBearing = inputBearing[0]/inputBearing[1]
+    else:
+        outputBearing = 0
+    return (outputLat,outputLon,outputBearing)
