1810063f9b7e232839eefff4c663b83564eeee8f
Tyler Richman <tyler.richman@erg.com>
2025-03-24 20:05:28 -0400

    Update setup.py
    Update with libraries needed for the application.

--- setup.py
+++ setup.py
@@ -6,27 +6,24 @@ import subprocess
 class App:
     def __init__(self):
         self.workspace_path = r""
-        self.config_file_path = r""
 
 if __name__ == "__main__":
     App = App()
 
-    #config = configparser.ConfigParser()
-    #config.read(App.config_file_path)
-    #Config = Config(config)
+    App.workspace_path = os.path.abspath(os.path.dirname(__file__))
+    
+    main_exe_folder_location = os.path.join(App.workspace_path, r"exe")
+    activate_venv_command = os.path.join(App.workspace_path, r"python-env\Scripts\activate.bat")
+    python_exe_location = os.path.join(App.workspace_path, r"python-env\Scripts\python.exe")
+    pip_exe_location = os.path.join(App.workspace_path, r"python-env\Scripts\pip.exe")
+    pyinstaller_exe_location = os.path.join(App.workspace_path, r"python-env\Scripts\pyinstaller.exe")
+    main_py_location = os.path.join(App.workspace_path, r"dev\main.py")
+    icon_location = os.path.join(App.workspace_path, r"misc\icon.png") #! Change with path of new icon.
+    output_exe_location = os.path.join(App.workspace_path, r"exe\dist\main.exe")
+    final_exe_location = os.path.join(App.workspace_path, r"EPA-Geolocated-Photograph-Log-DesktopGUI_0_0_0.exe")
 
-    main_exe_folder_location = os.path.join(App.workspace_folder, r"exe")
-    activate_venv_command = os.path.join(App.workspace_folder, r"python-env\Scripts\activate.bat")
-    python_exe_location = os.path.join(App.workspace_folder, r"python-env\Scripts\python.exe")
-    pip_exe_location = os.path.join(App.workspace_folder, r"python-env\Scripts\pip.exe")
-    pyinstaller_exe_location = os.path.join(App.workspace_folder, r"python-env\Scripts\pyinstaller.exe")
-    main_py_location = os.path.join(App.workspace_folder, r"dev\main.py")
-    icon_location = os.path.join(App.workspace_folder, r"dev\icons\icons8-f-67.png") #! Change with path of new icon.
-    output_exe_location = os.path.join(App.workspace_folder, r"exe\dist\main.exe")
-    final_exe_location = os.path.join(App.workspace_folder, r"EPA Geolocated Photograph Report Generator.exe")
-
-    if os.path.exists(os.path.join(App.workspace_folder, "python-env")):
-        shutil.rmtree(os.path.join(App.workspace_folder, "python-env"))
+    if os.path.exists(os.path.join(App.workspace_path, "python-env")):
+        shutil.rmtree(os.path.join(App.workspace_path, "python-env"))
 
     if os.path.exists(main_exe_folder_location):
         shutil.rmtree(main_exe_folder_location)
@@ -34,17 +31,19 @@ if __name__ == "__main__":
     if not os.path.exists(main_exe_folder_location):
         os.makedirs(main_exe_folder_location)
 
-    subprocess.run(['python', '-m', 'venv', os.path.join(App.workspace_folder, "python-env")], check=True)
+    subprocess.run(['python', '-m', 'venv', os.path.join(App.workspace_path, "python-env")], check=True)
 
     ### All Python libraries used in tool need to be included below ###
-    subprocess.run([activate_venv_command, "&&", python_exe_location, pip_exe_location, "install", "geopandas=1.0.1"], check=True)
     subprocess.run([activate_venv_command, "&&", python_exe_location, pip_exe_location, "install", "tk"], check=True)
     subprocess.run([activate_venv_command, "&&", python_exe_location, pip_exe_location, "install", "folium"], check=True)
     subprocess.run([activate_venv_command, "&&", python_exe_location, pip_exe_location, "install", "pillow"], check=True)
     subprocess.run([activate_venv_command, "&&", python_exe_location, pip_exe_location, "install", "selenium"], check=True)
+    subprocess.run([activate_venv_command, "&&", python_exe_location, pip_exe_location, "install", "pandas"], check=True)
+    subprocess.run([activate_venv_command, "&&", python_exe_location, pip_exe_location, "install", "python-docx"], check=True)
+    subprocess.run([activate_venv_command, "&&", python_exe_location, pip_exe_location, "install", "piexif"], check=True)
     subprocess.run([activate_venv_command, "&&", python_exe_location, pip_exe_location, "install", "pyinstaller"], check=True)
 
-    subprocess.run([activate_venv_command, "&&", "CD", main_exe_folder_location, "&&", pyinstaller_exe_location, "--onefile", f"--icon={icon_location}",main_py_location], check=True)
+    subprocess.run([activate_venv_command, "&&", "CD", main_exe_folder_location, "&&", pyinstaller_exe_location, "--onefile", f"--icon={icon_location}", main_py_location], check=True)
 
     shutil.copy(
         output_exe_location,
